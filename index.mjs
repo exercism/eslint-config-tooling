d531d826bd5c6de97ed1a1163b3b42bf57be2c80
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2024-07-29 14:42:56 +0200

    Format

--- index.mjs
+++ index.mjs
@@ -22,7 +22,6 @@ const plugin = {
   },
 };
 
-
 export default tsEslint.config(
   {
     plugins: {
@@ -68,94 +67,94 @@ export default tsEslint.config(
       // Enable boundary checks on TS files
       '@typescript-eslint/explicit-module-boundary-types': 'error',
 
-        // typescript rules
-        '@typescript-eslint/ban-tslint-comment': 'error',
-        '@typescript-eslint/consistent-type-assertions': [
-          'warn',
-          {
-            assertionStyle: 'as',
-            objectLiteralTypeAssertions: 'allow-as-parameter',
+      // typescript rules
+      '@typescript-eslint/ban-tslint-comment': 'error',
+      '@typescript-eslint/consistent-type-assertions': [
+        'warn',
+        {
+          assertionStyle: 'as',
+          objectLiteralTypeAssertions: 'allow-as-parameter',
+        },
+      ],
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { prefer: 'type-imports', disallowTypeAnnotations: false },
+      ],
+      '@typescript-eslint/explicit-function-return-type': [
+        'warn',
+        {
+          allowExpressions: false,
+          allowTypedFunctionExpressions: true,
+          allowHigherOrderFunctions: true,
+          allowDirectConstAssertionInArrowFunctions: true,
+          allowConciseArrowFunctionExpressionsStartingWithVoid: false,
+        },
+      ],
+      '@typescript-eslint/explicit-member-accessibility': [
+        'warn',
+        {
+          accessibility: 'no-public',
+          overrides: {
+            accessors: 'explicit',
+            constructors: 'no-public',
+            methods: 'explicit',
+            properties: 'explicit',
+            parameterProperties: 'off',
           },
-        ],
-        '@typescript-eslint/consistent-type-definitions': 'off',
-        '@typescript-eslint/consistent-type-imports': [
-          'warn',
-          { prefer: 'type-imports', disallowTypeAnnotations: false },
-        ],
-        '@typescript-eslint/explicit-function-return-type': [
-          'warn',
-          {
-            allowExpressions: false,
-            allowTypedFunctionExpressions: true,
-            allowHigherOrderFunctions: true,
-            allowDirectConstAssertionInArrowFunctions: true,
-            allowConciseArrowFunctionExpressionsStartingWithVoid: false,
-          },
-        ],
-        '@typescript-eslint/explicit-member-accessibility': [
-          'warn',
-          {
-            accessibility: 'no-public',
-            overrides: {
-              accessors: 'explicit',
-              constructors: 'no-public',
-              methods: 'explicit',
-              properties: 'explicit',
-              parameterProperties: 'off',
-            },
-          },
-        ],
-        '@typescript-eslint/member-delimiter-style': 'off',
-        '@typescript-eslint/method-signature-style': 'error',
-        '@typescript-eslint/naming-convention': 'warn',
-        '@typescript-eslint/no-base-to-string': 'warn',
-        '@typescript-eslint/no-confusing-non-null-assertion': 'warn',
-        '@typescript-eslint/no-confusing-void-expression': [
-          'warn',
-          { ignoreVoidOperator: true },
-        ],
-        '@typescript-eslint/no-implicit-any-catch': [
-          'warn',
-          { allowExplicitAny: true },
-        ],
-        '@typescript-eslint/no-invalid-void-type': 'warn',
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        '@typescript-eslint/no-parameter-properties': [
-          'warn',
-          {
-            allows: [
-              'private',
-              'protected',
-              'public',
-              'private readonly',
-              'protected readonly',
-              'public readonly',
-            ],
-          },
-        ],
-        '@typescript-eslint/no-unnecessary-condition': [
-          'off', // found false positives
-          { allowConstantLoopConditions: true },
-        ],
-        '@typescript-eslint/no-unused-vars': 'off',
-        '@typescript-eslint/no-use-before-define': [
-          'error',
-          {
-            functions: false,
-            typedefs: false,
-          },
-        ],
-        '@typescript-eslint/non-nullable-type-assertion-style': 'warn',
-        '@typescript-eslint/prefer-enum-initializers': 'warn',
-        '@typescript-eslint/prefer-includes': 'warn',
-        '@typescript-eslint/prefer-nullish-coalescing': 'warn',
-        '@typescript-eslint/prefer-optional-chain': 'warn',
-        '@typescript-eslint/prefer-readonly': 'warn',
-        '@typescript-eslint/prefer-reduce-type-parameter': 'warn',
-        '@typescript-eslint/prefer-ts-expect-error': 'error',
-        '@typescript-eslint/require-array-sort-compare': 'error',
-        '@typescript-eslint/require-await': 'off',
-        '@typescript-eslint/restrict-template-expressions': 'off',
+        },
+      ],
+      '@typescript-eslint/member-delimiter-style': 'off',
+      '@typescript-eslint/method-signature-style': 'error',
+      '@typescript-eslint/naming-convention': 'warn',
+      '@typescript-eslint/no-base-to-string': 'warn',
+      '@typescript-eslint/no-confusing-non-null-assertion': 'warn',
+      '@typescript-eslint/no-confusing-void-expression': [
+        'warn',
+        { ignoreVoidOperator: true },
+      ],
+      '@typescript-eslint/no-implicit-any-catch': [
+        'warn',
+        { allowExplicitAny: true },
+      ],
+      '@typescript-eslint/no-invalid-void-type': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-parameter-properties': [
+        'warn',
+        {
+          allows: [
+            'private',
+            'protected',
+            'public',
+            'private readonly',
+            'protected readonly',
+            'public readonly',
+          ],
+        },
+      ],
+      '@typescript-eslint/no-unnecessary-condition': [
+        'off', // found false positives
+        { allowConstantLoopConditions: true },
+      ],
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-use-before-define': [
+        'error',
+        {
+          functions: false,
+          typedefs: false,
+        },
+      ],
+      '@typescript-eslint/non-nullable-type-assertion-style': 'warn',
+      '@typescript-eslint/prefer-enum-initializers': 'warn',
+      '@typescript-eslint/prefer-includes': 'warn',
+      '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+      '@typescript-eslint/prefer-optional-chain': 'warn',
+      '@typescript-eslint/prefer-readonly': 'warn',
+      '@typescript-eslint/prefer-reduce-type-parameter': 'warn',
+      '@typescript-eslint/prefer-ts-expect-error': 'error',
+      '@typescript-eslint/require-array-sort-compare': 'error',
+      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
     },
   },
   {
@@ -186,5 +185,5 @@ export default tsEslint.config(
 
   // https://github.com/import-js/eslint-plugin-import/pull/3018
   // missing: import/* plugins
-  prettierConfig
+  prettierConfig,
 );
